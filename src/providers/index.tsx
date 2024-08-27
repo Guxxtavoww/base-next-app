0f8911d1b71ee58a1e25d03426d7a210469e65e8
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-08-27 16:32:11 -0300

    Chore: adding internationalization

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
 import { HydrationOverlay } from '@builder.io/react-hydration-overlay';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { TooltipProvider } from '@/components/ui/tooltip';
 
-import { TanstackProvider } from './tanstack-provider';
+export const queryClient = new QueryClient();
 
 export function Providers({
   children,
@@ -12,11 +13,11 @@ export function Providers({
 }: WithChildren<ThemeProviderProps>): JSX.Element {
   return (
     <HydrationOverlay>
-      <TanstackProvider>
+      <QueryClientProvider client={queryClient}>
         <NextThemesProvider {...props}>
           <TooltipProvider>{children}</TooltipProvider>
         </NextThemesProvider>
-      </TanstackProvider>
+      </QueryClientProvider>
     </HydrationOverlay>
   );
 }
