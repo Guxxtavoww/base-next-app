205a383846ed4295e7474fa3ba33ea672ad3ab60
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-10-26 15:34:46 -0300

    Feat: catch error function

--- src/utils/catch-error.util.ts
+++ src/utils/catch-error.util.ts
@@ -5,3 +5,11 @@ export async function catchError<T, E = Error>(
     .then((data) => [undefined, data] as [undefined, T])
     .catch((err) => [err as E]);
 }
+
+// const [error, test] = await catchError(new Promise(res => setTimeout(res, 10)))
+// if (error) {
+//      console.log('handle the error');
+    
+// } else {
+//     console.log(test)
+// }
